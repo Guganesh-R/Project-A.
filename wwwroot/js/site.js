ad36e2cb5782182e761a35a338f2fc12624cc703
Guganesh-R <111949367+Guganesh-R@users.noreply.github.com>
2023-03-08 14:28:26 +0530

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $("#create").click(function () {
+        alert("Congrats!!! Data Added Successfully");
+    });
+});
+
+$(document).ready(function () {
+    $("#edit").click(function () {
+        alert("Congrats!!! Data Edited Successfully");
+    });
+});
+
+$(document).ready(function () {
+    $("#delete").click(function () {
+        alert("Congrats!!! Data Deleted Successfully");
+    });
+});
